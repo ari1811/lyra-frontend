fea91b4714255512ebe11c0507aaa01bfdc1f0b6
ari1811 <ariberman2017@gmail.com>
2025-02-20 23:55:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: [],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
 };
+
+export default nextConfig;
+
