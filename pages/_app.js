740272fde13d9faba601a69f18549e4eee0cf73c
ari1811 <ariberman2017@gmail.com>
2025-02-20 23:23:40 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../components/Navbar';
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
